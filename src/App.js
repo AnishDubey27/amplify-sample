d9fa7596b4d155e9efe523669931cd006fb6cb17
Anish Dubey <anish.21scse1010057@galgotiasuniversity.edu.in>
2025-03-19 17:34:51 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to AWS Amplify!</h1>
+        <p>
+          This is a sample project deployed using AWS Amplify.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
